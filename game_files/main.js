3497222c1d45d4550bfbe8d174ad7eac0195f00a
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-08-17 16:05:53 +0000

    Add frogger level with tougher enviroment

--- game_files/main.js
+++ game_files/main.js
@@ -1,6 +1,7 @@
 import Scene1 from "./scene.js";
 import Scene2 from "./scene2.js";
 import Scene3 from "./scene3.js";
+import Scene4 from "./scene4.js";
 
 const config = {
   type: Phaser.AUTO,
@@ -13,7 +14,7 @@ const config = {
       debug: false,
     },
   },
-  scene: [Scene1, Scene2, Scene3],
+  scene: [Scene1, Scene2, Scene3, Scene4],
 };
 
 const game = new Phaser.Game(config);
