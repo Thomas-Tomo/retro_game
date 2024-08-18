3e709f33f8318f1dddf761bf777e266fcc3236e5
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-08-18 12:38:00 +0000

    Update main.js with scene5

--- game_files/main.js
+++ game_files/main.js
@@ -2,6 +2,7 @@ import Scene1 from "./scene.js";
 import Scene2 from "./scene2.js";
 import Scene3 from "./scene3.js";
 import Scene4 from "./scene4.js";
+import Scene5 from "./scene5.js";
 
 const config = {
   type: Phaser.AUTO,
@@ -14,7 +15,7 @@ const config = {
       debug: false,
     },
   },
-  scene: [Scene1, Scene2, Scene3, Scene4],
+  scene: [Scene1, Scene2, Scene3, Scene4, Scene5],
 };
 
 const game = new Phaser.Game(config);
